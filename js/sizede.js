b5f2d1368200a5d1c27211a8889739e9686c35c6
Bitstick <thebitstick@tfwno.gf>
2016-02-25 16:31:18 -0600

    I'm not good at math

--- js/sizede.js
+++ js/sizede.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $("#file").change(function () { 
      var iSize = Math.round($("#file")[0].files[0].size / 1024); 
-     if (iSize * 1024 * 1024 > 40 * 1024 * 1024) {
+     if (iSize > 40 * 1024 * 1024) {
          alert('Diese Datei ist über dem 40MB Limit, versuche es mit einer anderen Datei.');
          location.reload();
      }    
