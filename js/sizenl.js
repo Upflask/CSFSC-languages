52aea3912613e8fdce7d0d379d78027f44a08c6d
Bitstick <thebitstick@tfwno.gf>
2016-02-25 16:31:52 -0600

    I'm not good at math

--- js/sizenl.js
+++ js/sizenl.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $("#file").change(function () { 
      var iSize = Math.round($("#file")[0].files[0].size / 1024); 
-     if (iSize * 1024 * 1024 > 40 * 1024 * 1024) {
+     if (iSize > 40 * 1024 * 1024) {
          alert('Dit bestand is overschreed het 40MB limiet, probeer een ander bestand.');
          location.reload();
      }    
