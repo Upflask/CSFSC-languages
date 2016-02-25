5461c7472d23f00e94ad9733bb7849c605e9022d
Bitstick <thebitstick@tfwno.gf>
2016-02-25 11:51:41 -0600

    Create typede.js

---
+++ typede.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $("#file").change(function () 
+   { 
+     var ext = $('#file').val().split('.').pop().toLowerCase();
+     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass']) == -1)
+      {
+    	alert('Dieser Dateityp ist nicht erlaubt, bitte schaue Dir die Nutzungsbedingungen an.');
+        location.reload();
+} 
+  }); 
+});
