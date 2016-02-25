4727d3b9abf4b48921da1c9dc64a6abf22066031
Bitstick <thebitstick@tfwno.gf>
2016-02-25 11:49:43 -0600

    Create typenl.js

---
+++ typenl.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $("#file").change(function () 
+   { 
+     var ext = $('#file').val().split('.').pop().toLowerCase();
+     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass']) == -1)
+      {
+    	alert('Deze bestandsextensie is niet toegestaan, controleer de voorwaarden.');
+        location.reload();
+} 
+  }); 
+});
